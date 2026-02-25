c9de74264dc1c9d895704cd9cc7c178fa4ded739
MarcusDavidG <davidmarcus827@gmail.com>
2026-02-25 02:42:46 +0100

    feat(frontend): implement deposit service

--- frontend/src/services/deposit.ts
+++ frontend/src/services/deposit.ts
@@ -1,1 +1,12 @@
-export const depositService = {};
+import { openContractCall } from '@stacks/connect';
+
+export const depositService = {
+  async deposit(amount: number, lockPeriod: number) {
+    return openContractCall({
+      contractAddress: 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM',
+      contractName: 'bitsave',
+      functionName: 'deposit',
+      functionArgs: [/* args */],
+    });
+  }
+};
