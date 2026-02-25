964054f2ab8fd48d60aeae6915e27ca6dd622032
MarcusDavidG <davidmarcus827@gmail.com>
2026-02-25 02:42:48 +0100

    feat(frontend): implement withdrawal service

--- frontend/src/services/withdraw.ts
+++ frontend/src/services/withdraw.ts
@@ -1,1 +1,12 @@
-export const withdrawService = {};
+import { openContractCall } from '@stacks/connect';
+
+export const withdrawService = {
+  async withdraw() {
+    return openContractCall({
+      contractAddress: 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM',
+      contractName: 'bitsave',
+      functionName: 'withdraw',
+      functionArgs: [],
+    });
+  }
+};
